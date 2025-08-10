ad8fab9ca50588bf5d896d6cf512833e491b907a
sgavr1 <sanya.gavrilko60@gmail.com>
2025-08-10 14:05:33 +0200

    Init

---
+++ src/test/java/com/message/failure/messagefailure/MessagefailureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.message.failure.messagefailure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagefailureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
