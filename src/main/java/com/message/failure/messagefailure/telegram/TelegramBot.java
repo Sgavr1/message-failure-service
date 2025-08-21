3ca056bd3250a8fcbc22125d775a4221ee39cddb
sgavr1 <sanya.gavrilko60@gmail.com>
2025-08-21 10:55:15 +0200

    deploy v 0.2

--- src/main/java/com/message/failure/messagefailure/telegram/TelegramBot.java
+++ src/main/java/com/message/failure/messagefailure/telegram/TelegramBot.java
@@ -1,6 +1,5 @@
 package com.message.failure.messagefailure.telegram;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -10,9 +9,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class TelegramBot extends TelegramLongPollingBot {
     private final String botName;
 
-    public TelegramBot(@Value("${TELEGRAM_BOT_NAME}") String botName, @Value("${TELEGRAM_BOT_TOKEN}") String token){
-        super(token);
-        this.botName = botName;
+    public TelegramBot(){
+        super("8465197046:AAHloA-UX2cCa8wfugpEMZlQLtIN8qZ-oJs");
+        this.botName = "message_failure_bot";
     }
 
     @Override
