1a0201edd2613e930cb2f258ba455175137729f4
sgavr1 <sanya.gavrilko60@gmail.com>
2025-08-21 11:20:50 +0200

    send request in chat

--- src/main/java/com/message/failure/messagefailure/telegram/TelegramBot.java
+++ src/main/java/com/message/failure/messagefailure/telegram/TelegramBot.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     private final String botName;
 
-    public TelegramBot(){
+    public TelegramBot() {
         super("8465197046:AAHloA-UX2cCa8wfugpEMZlQLtIN8qZ-oJs");
         this.botName = "message_failure_bot";
     }
@@ -18,6 +19,16 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         System.out.println(update.getMessage().getText());
         SendMessage message = new SendMessage(update.getMessage().getChatId().toString(), "Бот временно не работает");
+
+        try {
+            execute(message);
+            message.setText("Подождите пару дней");
+            execute(message);
+            message.setText("Мне за это не платят");
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
