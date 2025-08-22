c08bcae42e3bfee2cba467bffd0bca25323b088a
sgavr1 <sanya.gavrilko60@gmail.com>
2025-08-22 11:28:16 +0200

    add telegramUser contructor and get set

--- src/main/java/com/message/failure/messagefailure/telegram/UserTelegramBot.java
+++ src/main/java/com/message/failure/messagefailure/telegram/UserTelegramBot.java
@@ -6,4 +6,28 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class UserTelegramBot {
     private User user;
     private Update update;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Update getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(Update update) {
+        this.update = update;
+    }
+
+    public UserTelegramBot(User user, Update update) {
+        this.user = user;
+        this.update = update;
+    }
+
+    public UserTelegramBot() {
+    }
 }
