f650d7a40833c560e36c88d4062e3885b34e5d83
sgavr1 <sanya.gavrilko60@gmail.com>
2025-08-22 11:27:19 +0200

    Add telegram user

---
+++ src/main/java/com/message/failure/messagefailure/telegram/UserTelegramBot.java
@@ -0,0 +1,9 @@
+package com.message.failure.messagefailure.telegram;
+
+import com.message.failure.messagefailure.model.entity.User;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class UserTelegramBot {
+    private User user;
+    private Update update;
+}
