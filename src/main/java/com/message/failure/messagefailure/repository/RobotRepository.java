97973d6844060ff72e45d2a03a097c3330a12bed
sgavr1 <sanya.gavrilko60@gmail.com>
2025-09-17 19:29:26 +0200

    Remove telegram api

--- src/main/java/com/message/failure/messagefailure/repository/RobotRepository.java
+++ src/main/java/com/message/failure/messagefailure/repository/RobotRepository.java
@@ -1,13 +1,7 @@
 package com.message.failure.messagefailure.repository;
 
-import com.message.failure.messagefailure.model.entity.Robot;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface RobotRepository {
-    List<Robot> getRobots();
-    Robot create(Robot robot);
-    Robot update(Robot robot);
 }
